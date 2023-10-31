075d2d4f61f255f909548e575b9e80ea01261e5a
abc <abc@example.com>
2023-10-31 21:32:20 +0000

    I have created a product,js file which links all the pictures in from the assets folder to each price and id for the product that we have

---
+++ src/products.js
@@ -0,0 +1,90 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+import product9 from "./assets/products/9.png";
+import product10 from "./assets/products/10.png";
+import product11 from "./assets/products/11.png";
+import product12 from "./assets/products/12.png";
+
+
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: " Code with Corgis",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: " Hacker Cat",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Hacker Coding",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Dear Coding",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Fox Coding",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Butterfly Coding",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Sloth Coding",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Lizard Coding",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Dragon Coding",
+    price: 40.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Tiger Coding",
+    price: 40.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "Lion Coding",
+    price: 40.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Llama Coding",
+    price: 40.0,
+    productImage: product12,
+  },
+];
