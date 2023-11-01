3ccb121669e6ef43ff23703980a26c2882274f35
abc <abc@example.com>
2023-11-01 19:14:13 +0000

    I have changed the currency sign to £ and changed the css file as the buttons and price for the top products were hidden

--- src/products.js
+++ src/products.js
@@ -72,13 +72,13 @@ export const PRODUCTS = [
   {
     id: 10,
     productName: "Tiger Coding",
-    price: 40.0,
+    price: 30.0,
     productImage: product10,
   },
   {
     id: 11,
     productName: "Lion Coding",
-    price: 40.0,
+    price: 50.0,
     productImage: product11,
   },
   {
