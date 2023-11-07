44b4a6c93d5f7e5c7065f42e15328619c437ee4c
symox <symoxn7@gmail.com>
2023-11-07 20:48:26 +0000

    stash fix DONT MERGE

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -8,6 +9,19 @@ import { Cart } from './pages/cart/cart';
 import { ShopContextProvider } from './context/shop-context';
 import PaymentIcons from './components/icons/icons';
 import Header from './components/Header'; 
+=======
+import "./App.css";
+import "./pages/shop/shop.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+import anime from 'animejs/lib/anime.es.js';
+
+const wrapper = document.getElementById("tiles");
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -28,4 +42,22 @@ function App() {
   );
 }
 
+// anime({
+//   targets: '.background-grid',
+//   scale: [
+//     {value: .1, easing: 'easeOutSine', duration: 500},
+//     {value: 1, easing: 'easeInOutQuad', duration: 1200}
+//   ],
+//   delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
+// });
+
+// anime({
+//   targets: '.addToCartBttn',
+//   translateX: 250,
+//   scale: 2,
+//   rotate: '1turn'
+// });
+
 export default App;
+
+
